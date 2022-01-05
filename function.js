4b2616c0e0e29b7e9756a926cd441cdda67b972d
GeonWooPaeng <gwpaeng@naver.com>
2022-01-05 16:53:52 +0900

    Add Kleisli Composition, Promise

--- function.js
+++ function.js
@@ -5,6 +5,8 @@ const curry =
   (a, ..._) =>
     _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
+const go1 = (a, f) => (a instanceof Promise ? a.then(f) : f(a));
+
 const map = curry((f, iter) => {
   let res = [];
   for (const a of iter) {
@@ -25,11 +27,24 @@ const reduce = curry((f, acc, iter) => {
   if (!iter) {
     iter = acc[Symbol.iterator]();
     acc = iter.next().value;
+  } else {
+    iter = iter[Symbol.iterator]();
   }
-  for (const a of iter) {
-    acc = f(acc, a);
-  }
-  return acc;
+  // for (const a of iter) {
+  //   // acc = f(acc, a);
+  //   //Promise 들어왔을 경우 해결
+  //   acc = acc instanceof Promise ? acc.then((acc) => f(acc, a)) : f(acc, a);
+  // }
+  return go1(acc, function recur() {
+    //유명함수 : 함수를 값으로 다루면서 함수의 이름을 짓는 기법
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      acc = f(acc, a);
+      if (acc instanceof Promise) return acc.then(recur);
+    }
+    return acc;
+  });
 });
 
 const go = (...args) => reduce((a, f) => f(a), args);
@@ -37,7 +52,3 @@ const pipe =
   (f, ...fs) =>
   (...as) =>
     go(f(...as), ...fs);
-
-
-
-    
